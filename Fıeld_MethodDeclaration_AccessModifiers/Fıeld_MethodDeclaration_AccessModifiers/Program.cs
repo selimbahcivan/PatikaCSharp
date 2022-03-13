b50b1332adf9a69a9606753d575fa151e2718133
YavuzSelim <y.selimbahcivan@gmail.com>
2022-03-13 17:39:27 +0300

    Method Declaration | Field | Access Modifiers added

---
+++ Fıeld_MethodDeclaration_AccessModifiers/Fıeld_MethodDeclaration_AccessModifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fıeld_MethodDeclaration_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal personal1 = new Personal();
+            personal1.name = "Selim";
+            personal1.surname = "Bahcıvan";
+            personal1.age = 24;
+            personal1.department = "Engineer";
+            personal1.PersonalInfo();
+        }
+    }
+    class Personal
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public string department;
+
+        public void PersonalInfo()
+        {
+            Console.WriteLine("Name : " +name);
+            Console.WriteLine("Surname : " + surname);
+            Console.WriteLine("Age : " +age);
+            Console.WriteLine("Department : " + department);
+        }
+    }
+}
